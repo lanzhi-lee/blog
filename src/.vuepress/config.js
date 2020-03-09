b1b2fa11e2e3dbe0cd2a79ccf558e9e6e975466e
李三思 <baibai_lee@163.com>
2020-03-09 13:25:08 +0800

    fix:更新信息卡片背景

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -107,7 +107,7 @@ module.exports = {
       // 卡片 header 的背景，可以使用图片，或者随机变化的图案（geopattern）
       headerBackground: {
         // 使用图片的 URL，如果设置了图片 URL，则不会生成随机变化的图案，下面的 useGeo 将失效
-        url: '/assets/img/bg.jpg',
+        // url: '/assets/img/bg.jpg',
 
         // 使用随机变化的图案，如果设置为 false，且没有设置图片 URL，将显示为空白背景
         useGeo: true,
