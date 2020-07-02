53198a00cebf777c2f80e091ebe326a0b8cb86c9
李三思 <baibai_lee@163.com>
2020-07-02 21:04:41 +0800

    feat: 更新工作流触发方式

---
+++ tag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+prefix="blogs-"
+
+function git-tag() {
+    git push
+    git pull --tags
+    local new_tag=$(echo ${prefix}-$(date +'%Y%m%d')-$(git tag -l "${prefix}mi-$(date +'%Y%m%d')-*" | wc -l | xargs printf '%02d'))
+    echo ${new_tag}
+    git tag ${new_tag}
+    git push origin $new_tag
+}
+
+git-tag;
